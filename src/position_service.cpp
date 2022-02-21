d9773d62f64f35f098066e63537b4ba64e03d64e
sh075 <73772653+sh075@users.noreply.github.com>
2022-02-21 03:50:59 -0800

    Update position_service.cpp

--- src/position_service.cpp
+++ src/position_service.cpp
@@ -1,18 +1,80 @@
+/**
+* \file position service.cpp
+* \brief This file impelements the position service
+* \author Shahrzad Eskandari Majdar
+* \version 1.0
+* \date 11/02/2022
+*
+* \details
+*   
+*  Subscribes to: <BR>
+*	 None
+*
+*  Publishes to: <BR>
+*	 None
+*
+*  Services: <BR>
+*    /position_server
+*
+*  Action Services: <BR>
+*    None
+*
+*  Description: <BR>
+*    This node implements the server that returns a random position and 
+* 	 orientation for the robot between given intervals.
+*/
+
+
+
+
+
 #include "ros/ros.h"
-#include "rt2_assignment1/RandomPosition.h"
+#include "rt2_assignment2/RandomPosition.h"
+
+/**
+ * \brief: It generates a random number
+ * \param M: the lower bound of the interval I want to have the random number in
+ * \param N: the upper bound of the interval I want to have the random number in
+ * 
+ * \return: the random number generated
+ * 
+ * This function uses the library function rand() to generate a random number and
+ *  then resizes it to be in the interval [M;N].
+ */
 
 
 double randMToN(double M, double N)
 {     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
 
+/**
+ * \brief: Called when I have a request from the client
+ * \param req: it is a message composed of the maximum and minimum of the intervalin both x and y
+ * \param res: it is a message composed of the random x and y coordinates and the orientation
+ * 
+ * \return: True
+ * 
+ * This function calls the function randMtoN to generate three random numbers,
+ * the x coordinate, the y coordinate and the orientation that is said to be
+ * requested between -pi and pi.
+ */
 
-bool myrandom (rt2_assignment1::RandomPosition::Request &req, rt2_assignment1::RandomPosition::Response &res){
+
+
+bool myrandom (rt2_assignment2::RandomPosition::Request &req, rt2_assignment2::RandomPosition::Response &res){
     res.x = randMToN(req.x_min, req.x_max);
     res.y = randMToN(req.y_min, req.y_max);
     res.theta = randMToN(-3.14, 3.14);
     return true;
 }
 
+/**
+ * \brief: The main function
+ * 
+ * \return: 0
+ * 
+ * This function initializes the ros node and the server /position_server,
+ * it then runs waiting for a request for the server.
+ */
 
 int main(int argc, char **argv)
 {
@@ -23,3 +85,4 @@ int main(int argc, char **argv)
 
    return 0;
 }
+
